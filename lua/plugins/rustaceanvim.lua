bee7740771fd5afa09ed2826a146f5e402405265
yoshijulas <yoshijulas@live.com>
2025-01-31 09:45:58 -0600

    Added some Args for clippy to ignore common lints
    Allowed cast_sign_loss, and cast_possible_wrap, when converting from one
    type to another where it can lose the sign

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -6,7 +6,13 @@ return {
         ["rust-analyzer"] = {
           check = {
             command = "clippy",
-            extraArgs = { "--", "-Wclippy::pedantic", "-Wclippy::nursery" },
+            extraArgs = {
+              "--",
+              "-Wclippy::pedantic",
+              "-Wclippy::nursery",
+              "-Aclippy::cast_sign_loss",
+              "-Aclippy::cast_possible_wrap",
+            },
           },
         },
       },
