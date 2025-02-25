aa8cc247d048cb6ff07dcbd931183c3e6c6233bf
yoshijulas <yoshijulas@live.com>
2025-02-24 20:02:23 -0600

    feat(nvim): add live preview plugin for html

---
+++ nvim/lua/plugins/live-preview.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "brianhuster/live-preview.nvim",
+    dependencies = {
+      -- You can choose one of the following pickers
+      "nvim-telescope/telescope.nvim",
+      "ibhagwan/fzf-lua",
+      "echasnovski/mini.pick",
+    },
+  },
+}
