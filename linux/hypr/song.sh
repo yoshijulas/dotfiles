d71dd3051cc96cd32da5822f503bd70dda0aadd4
yoshijulas <yoshijulas@live.com>
2025-04-09 13:09:38 -0600

    fix(hyprlock): fix player showing no lenght/duration when the player doesn't provide it

--- linux/hypr/song.sh
+++ linux/hypr/song.sh
@@ -8,4 +8,12 @@ elif [ "$status" = "Paused" ]; then
   status=""
 fi
 
-playerctl metadata -f "$status  {{title}} - {{artist}} [{{duration(position)}}/{{duration(mpris:length)}}]"
+duration=$(playerctl metadata -f "{{duration(position)}}" 2>/dev/null)
+
+if [ "$duration" = "0:00" ]; then
+  duration=""
+else
+  duration=$(playerctl metadata -f "[{{duration(position)}}/{{duration(mpris:length)}}]" 2>/dev/null)
+fi
+
+playerctl metadata -f "$status  {{title}} - {{artist}} $duration"
