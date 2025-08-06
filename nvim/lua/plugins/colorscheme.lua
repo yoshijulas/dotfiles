6f66c11170dc38f7d13e869b9975f3915239b23b
yoshijulas <yoshijulas@live.com>
2025-08-06 14:49:15 -0600

    feat(nvim): add everforest colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,21 @@
 return {
+  {
+    "https://github.com/sainnhe/everforest",
+    name = "everforest",
+    priority = 1000,
+    config = function()
+      vim.g.everforest_background = "medium"
+      vim.g.everforest_ui_contrast = "low"
+      -- vim.g.everforest_diagnostic_text_highlight = "1"
+      -- vim.g.everforest_diagnostic_line_highlight = "1"
+    end,
+  },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-macchiato",
+      -- colorscheme = "catppuccin-macchiato",
+      colorscheme = "everforest",
     },
   },
 }
