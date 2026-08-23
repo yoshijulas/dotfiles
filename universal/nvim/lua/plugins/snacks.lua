48c488f56cd30cf4a78ce354f535f1d0ef920098
yoshijulas <yoshijulas@live.com>
2026-08-23 11:26:24 -0600

    feat(nvim): disable snacks explorer and change layout of picker

--- universal/nvim/lua/plugins/snacks.lua
+++ universal/nvim/lua/plugins/snacks.lua
@@ -7,7 +7,16 @@ return {
       size = 8 * 1024 * 1024,
       --   enabled = false,
     },
+    explorer = {
+      enabled = true,
+      replace_netrw = false, -- Replace netrw with the snacks explorer
+      trash = true, -- Use the system trash when deleting files
+    },
     picker = {
+      layout = {
+        -- Change later to default if feels weird
+        preset = "ivy_split",
+      },
       sources = {
         explorer = {
           layout = {
