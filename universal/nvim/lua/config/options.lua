554de9fce9e92df27b76f6bfee32d571e2122114
yoshijulas <yoshijulas@live.com>
2026-08-23 11:26:24 -0600

    feat(nvim): disable new dir manager

--- universal/nvim/lua/config/options.lua
+++ universal/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable Neovim 0.11+ built-in dir.lua directory plugin
+vim.g.loaded_nvim_dir_plugin = 0
