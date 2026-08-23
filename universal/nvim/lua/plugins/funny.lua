d824369822fc2a6270da1a4c66ff0ecd4e853111
yoshijulas <yoshijulas@live.com>
2026-08-23 11:26:24 -0600

    feat(nvim): add wrapped and add cmd so it loads lazy CellularAutomaton

--- universal/nvim/lua/plugins/funny.lua
+++ universal/nvim/lua/plugins/funny.lua
@@ -1,11 +1,21 @@
 return {
-  "Eandrju/cellular-automaton.nvim",
-  keys = {
-    {
-      "<leader>fml",
-      mode = "n",
-      "<cmd>CellularAutomaton make_it_rain<CR>",
-      desc = "Cellular Automaton make_it_rain",
+  {
+    "Eandrju/cellular-automaton.nvim",
+    cmd = "CellularAutomaton",
+    keys = {
+      {
+        "<leader>fml",
+        mode = "n",
+        "<cmd>CellularAutomaton make_it_rain<CR>",
+        desc = "Cellular Automaton make_it_rain",
+      },
     },
+  },
+  {
+    -- :WrappedNvim
+    "aikhe/wrapped.nvim",
+    cmd = { "WrappedNvim" },
+    dependencies = { "nvzone/volt" },
+    opts = {},
   },
 }
