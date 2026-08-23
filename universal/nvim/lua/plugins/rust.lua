688ff77cf5761399a9d318ab3a4d83abefbf9d70
yoshijulas <yoshijulas@live.com>
2026-08-23 11:26:24 -0600

    feat(nvim): add more sane clippy config and add rustfmt nightly

--- universal/nvim/lua/plugins/rust.lua
+++ universal/nvim/lua/plugins/rust.lua
@@ -4,14 +4,22 @@ return {
     server = {
       default_settings = {
         ["rust-analyzer"] = {
+          rustfmt = {
+            extraArgs = { "+nightly" },
+          },
           check = {
             command = "clippy",
             extraArgs = {
               "--",
-              "-Wclippy::pedantic",
-              "-Wclippy::nursery",
-              "-Aclippy::cast_sign_loss",
-              "-Aclippy::cast_possible_wrap",
+              "-Wclippy::dbg_macro",
+              "-Wclippy::todo",
+              "-Wclippy::disallowed_macros",
+              "-Wclippy::pedantic", -- Enable extra-strict lints for idiomatic/clean code
+              "-Wclippy::nursery", -- Enable experimental/in-development Clippy lints
+              "-Aclippy::cast_sign_loss", -- Allow signed to unsigned casts (e.g., i32 -> u32, isize -> usize)
+              "-Aclippy::cast_possible_wrap", -- Allow casts that might wrap signs (e.g., u32 -> i32)
+              "-Aclippy::cast_possible_truncation", -- Allow larger to smaller int casts (e.g., u32 -> u8, usize -> u32)
+              "-Aclippy::cast_precision_loss", -- Allow int to float casts that may lose bits (e.g., usize -> f32)
             },
           },
         },
