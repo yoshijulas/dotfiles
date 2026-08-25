983e87eb3f4e47a441e7cb0b975c19f7461542ed
yoshijulas <yoshijulas@live.com>
2026-08-24 22:47:37 -0600

    feat(dotbot): vendor dotbot without submodules

--- install.ps1
+++ install.ps1
@@ -7,9 +7,6 @@ $DOTBOT_BIN = "bin/dotbot"
 $BASEDIR = $PSScriptRoot
 
 Set-Location $BASEDIR
-git -C $DOTBOT_DIR submodule sync --quiet --recursive
-git submodule update --init --recursive $DOTBOT_DIR
-
 foreach ($PYTHON in ('python', 'python3')) {
     # Python redirects to Microsoft Store in Windows 10 when not installed
     if (& { $ErrorActionPreference = "SilentlyContinue"
